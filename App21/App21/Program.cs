41660ed3342ade1ece0b0e79986fa915c8e0ceff
Karol Madej <karol.k.madej@gmail.com>
2023-02-20 16:51:27 +0100

    Zadanie nr. 4

--- App21/App21/Program.cs
+++ App21/App21/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var age = 17;
+var isFemale = false;
+
+
+if (isFemale)
+{
+    if(age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else if (name == "Ewa" && age == 33)
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+}
+else if (age < 18)
+{
+        Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else
+{
+    Console.WriteLine("Brak kryteriów");
+}
